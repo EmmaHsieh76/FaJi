7a93ead5bf965a590c4e990f8f3b2717b339d656
EmmaHsieh <z0905631590@gmail.com>
2024-02-17 00:32:23 +0800

    快速預訂刻板

--- front/src/views/front/ProductView.vue
+++ front/src/views/front/ProductView.vue
@@ -1,13 +1,91 @@
 <template>
-<v-container>
-  <v-row>
-    <v-col cols="12">
-      <h1>購物網</h1>
-      <v-divider></v-divider>
-    </v-col>
-    <v-col cols="12" md="6" lg="3"></v-col>
+  <v-container>
+    <v-row class="mt-16">
+      <v-col cols="3" class="d-flex justify-center">
+        <v-card
+          min-width="250"
+        >
+          <v-list>
+            <v-list-subheader>快速預訂</v-list-subheader>
+            <v-divider></v-divider>
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              :value="item"
+              color="third"
+              rounded="xl"
+            >
+            <v-list-item-title class="ps-5">{{item.text}}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-table>
+          <tr >
+            <td colspan="2">
+              <v-img
+              :width="350"
+              :height="350"
+              aspect-ratio="1/1"
+              cover
+              src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+              ></v-img>
+            </td>
+          </tr>
+          <tr><br></tr>
+          <tr class="d-flex justify-center">
+            <td>
+              <v-img
+            :width="70"
+            :height="70"
+            aspect-ratio="1/1"
+            class="ma-2 pa-2"
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+            ></v-img>
+            </td>
+            <td>
+              <v-img
+            :width="70"
+            :height="70"
+            aspect-ratio="1/1"
+            class="ma-2 pa-2"
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+            ></v-img>
+            </td>
+          </tr>
+        </v-table>
+      </v-col>
+      <v-col cols="4">
+        <div>
+          <h2>粉粿</h2>
+          <v-divider></v-divider>
+          <p class="mt-2">精選優質地瓜澱粉</p>
+          <p>口感Q彈有嚼勁</p>
+          <p>搭配古法熬煮的濃郁紅糖食用</p>
+          <p>變換吃法:搭配愛玉、鳳梨、牛奶等，以增添層次感和風味</p>
+          <br>
+          <br>
+          <p>重量: 約500g</p>
+          <p>
+            請於收到後立即放入冰箱冷藏保存，並於冷藏3天內食用完畢，以確保粉粿新鮮美味。
+          </p>
+          <br>
+          <v-text-field
+            v-model="selectedNumber"
+            width="50px"
+            type="number"
+            variant="outlined"
+            :min="0"
+            :max="10"
+            :step="1"
+          ></v-text-field>
+          <v-btn color="seventh" prepend-icon="mdi-cart"  @click="addCart">加入購物車</v-btn>
+        </div>
+      </v-col>
   </v-row>
-</v-container>
+  </v-container>
+
 </template>
 
 <script setup>
@@ -18,7 +96,19 @@ import { useSnackbar } from 'vuetify-use-dialog'
 const { api } = useApi()
 const createSnackbar = useSnackbar()
 
+const selectedNumber = ref(null)
+
 const products = ref([])
+
+const items = ref([
+  { text: '粉粿' },
+  { text: '米苔目' },
+  { text: '粉條' },
+  { text: '愛玉' },
+  { text: '鳳梨' },
+  { text: '仙草' },
+  { text: '杏仁' }
+])
 
 onMounted(async () => {
   try {
@@ -48,7 +138,7 @@ onMounted(async () => {
 </script>
 
 <style>
-*{
+th{
   color:#3f000f
 }
 </style>
